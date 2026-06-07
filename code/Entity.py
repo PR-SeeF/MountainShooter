6622a0f25e3b8d604a4c8c780bc50c989da7f5c6
SeeF <prseefeldt@gmail.com>
2026-06-06 22:33:45 -0300

    background level 1 parallax effect

--- code/Entity.py
+++ code/Entity.py
@@ -8,7 +8,7 @@ import pygame
 class Entity(ABC):
     def __init__(self, name: str, position: tuple):
         self.name = name
-        self.surf = pygame.image.load('./asset' + name + '.png')
+        self.surf = pygame.image.load('./asset/' + name + '.png')
         self.rect = self.surf.get_rect(left=0, top=0)
 
     @abstractmethod
--- code/Game.py
+++ code/Game.py
@@ -16,7 +16,7 @@ class Game:
             menu_return = menu.run()
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = level(self.window, 'level1', menu_return)
+                level = Level(self.window, 'level1', menu_return)
                 menu_return = level.run()
             elif menu_return == MENU_OPTION[3]:
                 pygame.display.flip()
